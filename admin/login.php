c283978e7e4f19d8351224c99027c370190a63ee
Adam <afaherty27@gmail.com>
2016-10-26 23:12:09 -0500

    login form created

---
+++ admin/login.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+	<form action="" method="">
+		<fieldset>
+			<legend>Log In Details</legend>
+			<label for="user">Username: </label>
+			<input type="text" name="user" value="" />
+			<label for="pass">Password: </label>
+			<input type="password" name="pass" />
+		</fieldset>
+			
+		<input type="submit" name="submit" value="Log In" />
+	</form>
+
+<?php
+
+?>
