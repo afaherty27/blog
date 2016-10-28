8f5831d495fd36e88a2b02cd66e248fe9edba105
Adam <afaherty27@gmail.com>
2016-10-28 01:12:17 -0500

    fixed passward VARCHAR length

--- blog.sql
+++ blog.sql
@@ -10,14 +10,13 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	user_id			INT 		PRIMARY KEY AUTO_INCREMENT,
 	username		VARCHAR(25) NOT NULL,
-	password		VARCHAR(15) NOT NULL,
+	password		VARCHAR(40) NOT NULL,
 	first_name		VARCHAR(25) NOT NULL,
 	last_name		VARCHAR(25) NOT NULL,
 	email			VARCHAR(35) NOT NULL,
 	join_date		DATETIME DEFAULT CURRENT_TIMESTAMP,
 	admin_access	TINYINT DEFAULT 0
 );
-
 
 /*blog post table*/
 CREATE TABLE blog_post (
