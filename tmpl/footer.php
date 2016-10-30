b982764ada422b7e8a41b0d0145c5f0d9e6618c9
Adam <afaherty27@gmail.com>
2016-10-30 17:40:39 -0500

    new footer with bootstrap

--- tmpl/footer.php
+++ tmpl/footer.php
@@ -1,4 +1,11 @@
 		<hr />
-		<p> Created by Adam Faherty, 2016 </p>
+		<!-- FOOTER -->
+		<div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+		  <div class="container">
+			<div class="navbar-text pull-left">
+			  <p>Created by Adam Faherty, 2016</p>
+			</div> <!-- close navbar-text -->
+		  </div> <!-- close container -->
+		</div> <!-- close navbar -->
 	</body>
 </html>
