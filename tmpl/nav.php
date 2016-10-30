c7562353119c9a60c7f72eb573ddec0b7df08830
Adam <afaherty27@gmail.com>
2016-10-30 17:48:03 -0500

    coded navigation menu bar

---
+++ tmpl/nav.php
@@ -0,0 +1,29 @@
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"> Toggle Navigation</span> <!--rh button-->
+        <span class="icon-bar"></span> <!-- 3 lines to make icon bar -->
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button> <!-- END BUTTON-->
+      <li><a class="navbar-brand" href="index.php">BLOG</a></li><!-- Title of page -->
+    </div> <!-- END NAVBAR-HEADER-->
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right"> <!-- align on right properly -->
+        <li class="active"><a href="#">HOME</a></li>
+        <li class="dropdown">
+          <a href="#"
+             class="dropdown-toggle"
+             data-toggle="dropdown">LOGIN<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">USER</a></li>
+            <li class="divider"></li> <!-- divides 2 sections of dropdown -->
+            <li class="dropdown-header"></li><!--menu title -->
+            <li><a href="#">ADMIN</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div> <!-- END NAVBAR COLLAPSE -->
+  </div><!--END CONTAINER-->
+</div> <!-- END NAVBAR -->
