fa25a9a5152b731d24c34d60002c703fc1a4b928
afaherty27 <afaherty27@gmail.com>
2016-10-31 16:17:53 -0500

    refactor directory

---
+++ admin/admintmpl/adminnav.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-3 col-lg-2">
+			<nav class="nav navbar-default navbar-fixed-side">
+				<ul class="nav nav-pills nav-stacked">
+					<li><a id="new">New Post</a></li>
+					<li><a href="#">Edit posts</a></li>
+					<li><a href="#">Publish Posts</a></li>
+				</ul>
+			</nav><!--end navbar -->
+		</div><!--end col-sm-1 col-lg-2 -->
+		<div id="adminContent" class="col-sm-2 col-lg-4">
+			
+		</div><!--end col-sm-9 col-lg-10-->
+		<div id="stats" class="col-sm-2 col-lg-4">
+			<h1>stats</h1>
+		</div>
+	</div><!--end row -->
+</div><!--end container-fluid ->
