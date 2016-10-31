64ee32cd98ef1b6f9c6d9bcaeca969f805aa68f5
afaherty27 <afaherty27@gmail.com>
2016-10-31 16:59:27 -0500

    post new blog to db started

--- admin/index.php
+++ admin/index.php
@@ -1,36 +1,28 @@
 <?php
 	require_once('../req/startsession.php');
 	require_once('../req/appvars.php');
+	
+	
+	//require user to be logged in, else redirect to login page
+	if (isset($_SESSION['user_id']))
+	{	
 	$title = 'Admin Home Page';
 	$jsfile = '../js/admin.js';
 	require_once('../tmpl/head.php');
 	require_once('../tmpl/nav.php');
 	
-	//require user to be logged in, else redirect to login page
-	if (isset($_SESSION['user_id']))
-	{	
-?>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-3 col-lg-2">
-				<nav class="nav navbar-default navbar-fixed-side">
-					<ul class="nav nav-pills nav-stacked">
-						<li><a id="new">New Post</a></li>
-						<li><a href="#">Edit posts</a></li>
-						<li><a href="#">Publish Posts</a></li>
-					</ul>
-				</nav><!--end navbar -->
-			</div><!--end col-sm-1 col-lg-2 -->
-			<div id="adminContent" class="col-sm-2 col-lg-4">
-				
-			</div><!--end col-sm-9 col-lg-10-->
-			<div id="stats" class="col-sm-2 col-lg-4">
-				<h1>stats</h1>
-			</div>
-		</div><!--end row -->
-	</div><!--end container-fluid ->
-	
+?>	
+		<div class="container">
+			<div class="row">
 <?php
+		require_once('admintmpl/adminnav.php');
+?>
+			<div id="adminContent" class="col-sm-2 col-lg-4">
+				<h1>stats</h1>
+			</div><!--end col-sm-9 col-lg-10-->
+			</div><!--end row -->
+		</div><!--end container-fluid ->
+<?php		
 	}
 	else
 	{
@@ -38,14 +30,6 @@
 	}
 	
 	require_once('../tmpl/footer.php');
-	
-	/*
-	<ul class="nav nav-pills nav-stacked">
-		<li><a href="#newpost" data-toggle="modal">new post</a></li>
-		<li><a href="#">Edit posts</a></li>
-		<li><a href="#">Publish Posts</a></li>
-	</ul>
-	*/
 ?>
 
 
--- admin/newpost.php
+++ admin/newpost.php
@@ -1,21 +1,68 @@
-<form id="newpost" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-	<h4>New Blog Post</h4>
-	<div class="form-group">
-		<label for="title">Title: </label><br />
-		<input type="text" class="form-control" 
-			   name="title" value="<?php if(!empty($title)) echo $title; ?>" /><br />
-		<label for="description">Description: </label><br />
-		<input type="text" class="form-control" 
-			   name="description" value="<?php if(!empty($description)) echo $description; ?>"/><br />
-		<label for="">Content: </label><br />
-		<textarea name="content" class="form-control" 
-				  rows="15" cols="100" value="<?php if(!empty($content)) echo $content; ?>">
-		</textarea><br />
-		<label><input type="checkbox" value="1"> Publish</label>
-	</div>
-	<input type="submit" id="submitnewpost" class="btn btn-primary" 
-		   value="ADD POST" name="submit" />
-	<a class="btn btn-primary" data-dismiss="modal">Close</a><br />
-	<small>REMINDER: POST WILL ONLY DISPLAY IF 'PUBLISH' IS SELECTED</small>
-</form>
+<?php
+	require_once('../req/startsession.php');
+	require_once('../req/appvars.php');
+	require_once('../req/connectvars.php');
+	
+	
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+	
+	if(isset($_POST['submit']))
+	{
+		$user_id 	 = $_SESSION['user_id'];
+		$blogtitle   = mysqli_real_escape_string($conn, trim($_POST['title']));
+		$description = mysqli_real_escape_string($conn, trim($_POST['description']));
+		$content 	 = mysqli_real_escape_string($conn, trim($_POST['content']));
+		
+		if(!empty($blogtitle) && !empty($description) && 
+				!empty($content))
+		{
+			//insert query
+			$insert = "INSERT INTO blog_post (user_id, title, description, content, post_date) " .
+					"VALUES ('$user_id', '$blogtitle', '$description', '$content', NOW())";
+			//execute query
+			mysqli_query($conn, $insert) 
+					or die('Error adding post to the database');
+			//exit
+			exit();
+		}//end validation
+		
+		//close connection
+		mysqli_close($conn);
+	}
+	
 
+	$title = 'Write New Post';
+	$jsfile = '../js/admin.js';
+	require_once('../tmpl/head.php');
+	require_once('../tmpl/nav.php');
+?>
+
+	<div class="container">
+		<div class="row">
+			<?php require_once('admintmpl/adminnav.php'); ?>
+			<form id="newpost" method="post" class="col-sm-2 col-lg-4"
+				  action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+				  <h4><?php echo $title; ?></h4>
+				<div class="form-group">
+					<label for="title">Title: </label><br />
+					<input type="text" class="form-control" name="title" 
+						   value="<?php if(!empty($blogtitle)) echo $blogtitle; ?>" />
+					<label for="description">Description: </label>
+					<input type="text" class="form-control" name="description"
+						    value="<?php if(!empty($description)) echo $description; ?>"/>
+					<label for="">Content: </label><br />
+					<textarea name="content" class="form-control" rows="15" cols="100"
+							  value="<?php if(!empty($content)) echo $content; ?>">
+					</textarea>
+					<label><input type="checkbox" value="1"> Publish</label>
+				</div>
+				<input type="submit" id="submitnewpost" class="btn btn-primary" 
+					   value="ADD POST" name="submit" />
+				<a class="btn btn-primary" data-dismiss="modal">Close</a><br />
+				<small>REMINDER: POST WILL ONLY DISPLAY IF 'PUBLISH' IS SELECTED</small>
+			</form>
+		</div> <!-- end row -->
+	</div><!-- end container -->
+<?php
+	require_once('../tmpl/footer.php');
+?>
